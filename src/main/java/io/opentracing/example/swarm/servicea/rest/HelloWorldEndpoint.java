dc7d45322b1b3f67551c8b306728d95f30be0fd5
Juraci Paixão Kröhling <juraci@kroehling.de>
2017-05-03 14:12:25 +0200

    Fixed extraction of span context

--- src/main/java/io/opentracing/example/swarm/servicea/rest/HelloWorldEndpoint.java
+++ src/main/java/io/opentracing/example/swarm/servicea/rest/HelloWorldEndpoint.java
@@ -1,14 +1,21 @@
 package io.opentracing.example.swarm.servicea.rest;
 
 
+import javax.inject.Inject;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
 import org.hawkular.apm.client.opentracing.APMTracer;
 
+import io.opentracing.SpanContext;
 import io.opentracing.Tracer;
+import io.opentracing.contrib.web.servlet.filter.HttpServletRequestExtractAdapter;
+import io.opentracing.propagation.Format;
 import io.opentracing.util.GlobalTracer;
 
 
@@ -19,13 +26,20 @@ public class HelloWorldEndpoint {
         GlobalTracer.register(new APMTracer());
 	}
 
+	@Context HttpServletRequest request;
+
 	@GET
 	@Produces("text/plain")
 	public Response doGet() {
-		System.out.println(GlobalTracer.get());
-
 		Tracer tracer = GlobalTracer.get();
-		tracer.buildSpan("myspan").start().setTag("component", "mycomponent").setOperationName("theoperation").close();
+		SpanContext spanContext = tracer.extract(Format.Builtin.HTTP_HEADERS, new HttpServletRequestExtractAdapter(request));
+		tracer
+				.buildSpan("myspan")
+				.asChildOf(spanContext)
+				.start()
+				.setTag("component", "mycomponent")
+				.setOperationName("theoperation")
+				.close();
 
 		return Response.ok("Hello from WildFly Swarm!").build();
 	}
